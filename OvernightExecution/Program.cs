76a8a5cc5f16ccb68936f1065362fbc6b66eb59f
wadaskah <wadaskah@BUPA-DM172>
2019-05-22 15:05:17 +0530

    Add project files.

---
+++ OvernightExecution/Program.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OvernightExecution
+{
+    [TestFixture]
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+        }
+    }
+}
