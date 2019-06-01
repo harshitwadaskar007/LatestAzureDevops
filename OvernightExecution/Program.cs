92d3b1209277f263d5377e72f60bf348f90b2196
harshitwadaskar007 <50833271+harshitwadaskar007@users.noreply.github.com>
2019-06-01 13:56:11 +0530

    edited

--- OvernightExecution/Program.cs
+++ OvernightExecution/Program.cs
@@ -23,46 +23,38 @@ namespace OvernightExecution
         public void FirstTest()
         {
 
-            int first = 1;
-            int second = 2;
-            int sum = first + second;
-            Assert.IsTrue(sum == 3);
-            //string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            //var chromeOptions = new ChromeOptions();
-            //chromeOptions.AddArguments("headless");
-            //IWebDriver driver = new ChromeDriver(path,chromeOptions);
-            //driver.Navigate().GoToUrl("https://www.google.com/");
-            //driver.Close();
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+            IWebDriver driver = new ChromeDriver(path, chromeOptions);
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.Close();
         }
 
         [Test]
         public void SecondTest()
         {
-            int first = 3;
-            int second = 7;
-            int sum = first + second;
-            Assert.IsTrue(sum == 10 );
-            //string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            //var chromeOptions = new ChromeOptions();
-            //chromeOptions.AddArguments("headless");
-            //IWebDriver driver = new ChromeDriver(path, chromeOptions);
-            //driver.Navigate().GoToUrl("https://www.google.com/");
-            //driver.Close();
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+            IWebDriver driver = new ChromeDriver(path, chromeOptions);
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.Close();
         }
 
-        [Test]
-        public void ThirdTest()
-        {
-            int first = 3;
-            int second = 7;
-            int sum = first + second;
-            Assert.IsTrue(sum == 1);
-            //string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            //var chromeOptions = new ChromeOptions();
-            //chromeOptions.AddArguments("headless");
-            //IWebDriver driver = new ChromeDriver(path, chromeOptions);
-            //driver.Navigate().GoToUrl("https://www.google.com/");
-            //driver.Close();
-        }
+        //[Test]
+        //public void ThirdTest()
+        //{
+        //    int first = 3;
+        //    int second = 7;
+        //    int sum = first + second;
+        //    Assert.IsTrue(sum == 1);
+        //    //string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        //    //var chromeOptions = new ChromeOptions();
+        //    //chromeOptions.AddArguments("headless");
+        //    //IWebDriver driver = new ChromeDriver(path, chromeOptions);
+        //    //driver.Navigate().GoToUrl("https://www.google.com/");
+        //    //driver.Close();
+        //}
     }
 }
